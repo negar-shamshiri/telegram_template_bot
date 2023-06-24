2407fd3c88b1c317d53cf390bcf84ff11541eb2b
Negar <negarshamshiri@yahoo.com>
2023-06-24 21:51:09 +0430

    update files

--- src/bot.py
+++ src/bot.py
@@ -6,35 +6,37 @@ from loguru import logger
 from src.constants import keyboards
 from src.utils.io import write_json
 
+bot = telebot.TeleBot(os.environ['TEMPLATE_TBOT_TOKEN'])
+
 
 class Bot:
     """Template for Telegram Bot
     """
-    def __init__(self):
-        self.bot = telebot.TeleBot(os.environ['TEMPLATE_TBOT_TOKEN'])
-        self.send_welcome = self.bot.message_handler(
-            commands=['start', 'help']
-        )(self.send_welcome)
-        self.echo_all = self.bot.message_handler(
-            func=lambda message: True
-        )(self.echo_all)
+    def __init__(self, telebot):
 
-    def run(self):
+        self.bot = telebot
+
+        # register handlers
+        self.handlers()
+
+        # run bot
         logger.info("Bot is running...!")
         self.bot.infinity_polling()
 
-    def send_welcome(self, message):
-        self.bot.reply_to(message, "Howdy, how are you doing?")
+    def handlers(self):
+        @self.bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            self.bot.reply_to(message, "Howdy, how are you doing?")
 
-    def echo_all(self, message):
-        write_json(message.json, 'messages')
-        self.bot.send_message(
-            message.chat.id, message.text,
-            reply_markup=keyboards.main
-        )
+        @self.bot.message_handler(func=lambda message: True)
+        def echo_all(message):
+            write_json(message.json, 'messages')
+            self.bot.send_message(
+                message.chat.id, message.text,
+                reply_markup=keyboards.main
+            )
 
 
 if __name__ == '__main__':
     logger.info('Bot started!')
-    bot = Bot()
-    bot.run()
+    bot = Bot(bot)
